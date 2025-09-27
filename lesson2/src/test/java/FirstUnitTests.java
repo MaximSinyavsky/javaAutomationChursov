1d0d3df17827e0fa261716d4c98a56941068c001
MaximSinyavsky <razarsinet1991@gmail.com>
2025-09-27 23:18:57 +0200

    update lesson2

---
+++ lesson2/src/test/java/FirstUnitTests.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstUnitTests {
+    @Test
+    @DisplayName("Проверка суммы")
+    @Tag("UnitTest")
+    void sumNumbersTest(){
+        //  AAA -> Arrange -> Act -> Assert (Разделяются пустой строкой)
+        // Arrange
+        int a = 3;
+        int b = 2;
+        // Act
+        int actualSum = a+b;
+        int expactedSum = 5;
+        //Assert
+        assertEquals(expactedSum, a+b);
+    }
+    @Test
+    @DisplayName("Проверка true / false")
+    void assertTrueFalseTest(){
+        int a = 2+2;
+        int b = 4;
+        assertTrue(a == b);
+        assertFalse(a !=b);
+    }
+}
