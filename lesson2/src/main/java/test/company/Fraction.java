c88cede71f3ce277e970b667d3fb1086ce755398
MaximSinyavsky <razarsinet1991@gmail.com>
2025-09-28 23:19:59 +0200

    update lesson2

---
+++ lesson2/src/main/java/test/company/Fraction.java
@@ -0,0 +1,53 @@
+package test.company;
+
+public class Fraction {
+    Integer num;
+    Integer den;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public Integer getDen() {
+        return den;
+    }
+
+    public Fraction(Integer num, Integer den) {
+        if (num == null || den == null) {
+            throw new NullPointerException("Values should be not null");
+        }
+        if (den == null) {
+            throw new NullPointerException("Cannot devide zero");
+        }
+        this.num = num;
+        this.den = den;
+    }
+
+    public static Fraction sum(Fraction first, Fraction second) {
+        long bigCommonDen = ((long) first.getDen() * (long) second.getDen());
+        if (bigCommonDen > Integer.MAX_VALUE) {
+            throw new RuntimeException("Common denominator is too big!");
+        }
+        Integer commonDen = first.getDen() * second.getDen();
+        Integer sumNum = first.getNum() * second.getDen() + second.getNum() * first.getDen();
+        return new Fraction(sumNum, commonDen);
+    }
+
+    @Override
+    public String toString() {
+        return num + "/" + den;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return Object.equals(num, fraction.num) && Object.equals(den, fraction.den);
+    }
+
+    @Override
+    public int hashCode() {
+        return Object.hash(num, den);
+    }
+}
