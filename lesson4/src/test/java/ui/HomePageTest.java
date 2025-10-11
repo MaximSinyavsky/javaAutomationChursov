78733df79638dccca3ac923063223d26e5efaf93
MaximSinyavsky <razarsinet1991@gmail.com>
2025-10-11 13:39:06 +0200

    add lesson4

--- lesson4/src/test/java/ui/HomePageTest.java
+++ lesson4/src/test/java/ui/HomePageTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HomePageTest {
